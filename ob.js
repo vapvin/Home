e1e36978f34b463f8df9b702e04d3c3139b0f515
vapvin <tpehdj12@daum.net>
2019-11-02 18:26:15 +0900

    Promise

--- ob.js
+++ ob.js
@@ -52,10 +52,48 @@ console.log(text); */
 	console.log(person.name);
 } */
 
-const calculateCircleArea = (r = 1) => {
+/* const calculateCircleArea = (r = 1) => {
 	return Math.PI * r * r;
 };
 
 const area = calculateCircleArea();
 
-console.log(area);
+console.log(area); */
+
+/* const object = { a: 1 };
+
+function print({ a, b = 2 }) {
+	console.log(a);
+	console.log(b);
+}
+
+print(object); */
+
+/* const myPromise = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve('result');
+	}, 1000);
+});
+
+myPromise.then((result) => {
+	console.log(result);
+}); */
+
+function increaseAndPrint(n) {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			const value = n + 1;
+			if (value === 5) {
+				const error = new Error();
+				error.name = 'ValueIsFiveError';
+				reject(error);
+				return;
+			}
+			console.log(value);
+			resolve(value);
+		}, 1000);
+	});
+}
+increaseAndPrint(0).then((n) => {
+	console.log('result: ', n);
+});
