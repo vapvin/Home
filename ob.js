8bc28cab6bb3927df79f68283a498470f3303e56
vapvin <tpehdj12@daum.net>
2019-11-02 17:27:37 +0900

    Today is 1102

---
+++ ob.js
@@ -0,0 +1,30 @@
+class Food {
+	constructor(name) {
+		this.name = name;
+		this.brands = [];
+	}
+
+	addBrand(brand) {
+		this.brands.push(brand);
+	}
+	print() {
+		console.log(`${this.name} 을 파는 음식점들:`);
+		console.log(this.brands.join(','));
+	}
+}
+
+const pizza = new Food('피자');
+
+pizza.addBrand('피자헛');
+pizza.addBrand('피자 스쿨');
+pizza.addBrand('도미노 피자');
+
+const chicken = new Food('치킨');
+
+chicken.addBrand('굽네');
+chicken.addBrand('또레오레');
+chicken.addBrand('BBQ');
+chicken.addBrand('교촌');
+
+pizza.print();
+chicken.print();
