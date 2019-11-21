f81fbcb68281959b217bcc9c2888e3cb04bd9beb
vapvin <tpehdj12@daum.net>
2019-11-21 21:27:49 +0900

    ToDo List Study For Today and Tomorrow

---
+++ todo.js
@@ -0,0 +1,7 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+let toDos = [];
